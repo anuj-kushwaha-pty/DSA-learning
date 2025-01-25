270f7e588943ac510faadbf74f6a70f36824823a
anuj-kushwaha-pty <119887154+anuj-kushwaha-pty@users.noreply.github.com>
2025-01-25 18:22:55 +0530

    Create 2_ compare two no.s

---
+++ 2_ compare two no.s
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cout<< "value of a is : "<< a <<endl;
+    cin>>a;
+    cout<< "Value of b is:"<< b <<endl;
+    cin>>b;
+
+    if(a>b){
+          cout<<"A is greater than b"<<endl;
+          }
+    if(b>a){
+          cout<<"B is greater than a"<<endl;
+          }
+    else{
+          cout<<"A & b  are equal"<<endl;
+         }
+         
+    
+
+}
